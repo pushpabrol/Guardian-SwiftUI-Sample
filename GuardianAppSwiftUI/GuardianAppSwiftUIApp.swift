08be1a1f1e9fe3d461d985363ca10e6e75ad86c6
pushpabrol <pushp.abrol@gmail.com>
2023-05-19 23:01:32 -0400

    Initial Commit

---
+++ GuardianAppSwiftUI/GuardianAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuardianAppSwiftUIApp.swift
+//  GuardianAppSwiftUI
+//
+//  Created by Pushp Abrol on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GuardianAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
